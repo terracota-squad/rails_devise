f82141a6fa07a397f5db125e28fe334d85f0b53b
Squad Terracota <squadterracota@gmail.com>
2026-09-17 20:43:01 -0300

    3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,29 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    if params[:locale]
-      cookies[:locale] = params[:locale]
+    requested_locale = params[:locale]&.downcase
+
+    if requested_locale.present? && locale_valid?(requested_locale)
+      cookies[:locale] = requested_locale
     end
 
-    if cookies[:locale]
-      if I18n.locale != cookies[:locale]
-        I18n.locale = cookies[:locale]
-      end
+    I18n.locale = extract_locale
+  end
+
+  private
+
+  def extract_locale
+    current_cookie = cookies[:locale]&.downcase
+
+    if current_cookie.present? && locale_valid?(current_cookie)
+      I18n.available_locales.find { |l| l.to_s.downcase == current_cookie }
+    else
+      I18n.default_locale
     end
+  end
+
+  def locale_valid?(locale)
+    available = I18n.available_locales.map { |l| l.to_s.downcase }
+    available.include?(locale.to_s.downcase)
   end
 end
